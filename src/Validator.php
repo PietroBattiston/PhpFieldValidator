cb91d773447fef4e5cb0666605043ab52cc061db
bttstn <pietro.battiston@gmail.com>
2020-12-28 17:56:48 +0000

    a field can be sanitized and an email can be recognized + tests

--- src/Validator.php
+++ src/Validator.php
@@ -4,25 +4,50 @@
 	class Validator {
 
 		public $name;
-    	private $content;
+    	public $content;
     	private $required;
 
+    	public function __construct() {
+    		$this->required = FALSE;
+    	}
+
+    	
 		public function fieldName($name) {
 			$this->name = $name;
+			$this->getContent();
 			return $this;
+		}
+
+
+		public function sanitize() {
+      		// We check if the Content is not empty and if is a string
+		    if ($this->content && $this->isString($this->content)) {
+		          $this->content = filter_var($this->content, FILTER_SANITIZE_STRING,FILTER_FLAG_STRIP_HIGH);
+		       	
+		       	return $this;
+		    }
+
+      		return $this;
 		}
 
 		public function isString(string $string) {
+			// We check if the input is a string. It returns a bool
 			return is_string($string);
 		}
 
-		public function sanitize(string $string) {
-			if ($string) {
-				$sanitized = filter_var($string, FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_HIGH);
-				return $sanitized;
-			}
+		private function getContent() {
+			// We get the Body Content
+      		$this->content = $_POST[$this->name];
+		}
 
-			return $this;
+		public function isEmail(string $email) {
+			// We check if the given input is a valid email
+			return (boolean) filter_var($email, FILTER_VALIDATE_EMAIL);
+		}
+
+		public function required() {
+			$this->required = TRUE;
+			return $this->required;
 		}
 
 
--- tests/UnitTest.php
+++ tests/UnitTest.php
@@ -1,13 +1,13 @@
 <?php 
 	declare(strict_types=1);
 	use PHPUnit\Framework\TestCase;
+	use PHPUnit\Framework\MockObject\MockObject;
+
 
 		// Our script must:
 		// Check if the field exists
 		// Check input type (string, file, etc)
 		// Set a field as required
-		// Sanitize strings OK
-		// Verify email format
 		// Verify telephone numbers format
 		// Verify Postcodes format
 		// Verify Date of birth
@@ -15,11 +15,20 @@
 		// Accepts only given images extentions
 
 
+				//OK
+		// Sanitize strings OK
+		// Verify email format
+
+	
+
 	class UnitTest extends TestCase {
 
+	   
 		public function test_a_field_name_can_be_set(): void {
 			$validator = new Validator;
-	       	$this->assertEquals('Surname', $validator->fieldName($_SERVER['POST'] = 'Surname')->name);
+			$_POST['name'] = "string";
+
+	       	$this->assertEquals('name', $validator->fieldName('name')->name);
 				
 		}
 
@@ -32,11 +41,30 @@
     	}
 
     	public function test_a_string_can_be_sanitized(): void {
-	        $string = "<alert>String</alert>";
 	        $validator = new Validator;
+	        $_POST['name'] = "<alert>String</alert>";
+	        $validator->name = 'name';
+	        $validator->content = $_POST['name'];
 
-	        $this->assertNotEquals($string, $validator->sanitize($string));
-	        $this->assertEquals("String", $validator->sanitize($string));
+	        $this->assertNotEquals($validator->content, $validator->sanitize($validator->content)->content);
+	        $this->assertEquals("String", $validator->sanitize($validator->content)->content);
     	}
 
+    	public function test_an_email_can_be_recognized(): void {
+   			$validator = new Validator;
+   			$email = "email@email.com";
+   			
+   			$this->assertIsBool($validator->isEmail($email));
+	       	$this->assertEquals(true, $validator->isEmail($email));
+   		}
+
+   		public function test_a_field_can_be_required(): void {
+   			$validator = new Validator;
+   			
+   			$this->assertIsBool($validator->required());
+	       	$this->assertEquals(true, $validator->required());
+   		}
+
+   		
    	}
+
